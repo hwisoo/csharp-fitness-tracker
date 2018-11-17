e700d9546228728f38a3f0f82416a2c914527597
James Cho <hwisoocho@gmail.com>
2018-11-16 22:05:31 -0800

    while loop

--- Exercise.cs
+++ Exercise.cs
@@ -7,19 +7,24 @@ namespace Fitness
     static void Main()
     {
       int runningTotal = 0;
+      bool keepGoing = true;
 
-      // Prompt user for minutes exercised
-      Console.WriteLine("Enter how many minutes you exercised: ");
-      string entry = Console.ReadLine();
+      while (keepGoing)
+      {
+        // Prompt user for minutes exercised
+        Console.WriteLine("Enter how many minutes you exercised: ");
+        string entry = Console.ReadLine();
 
-      // Add minutes exercised to total
-      int minutes = int.Parse(entry);
-      runningTotal = runningTotal + minutes;
+        // Add minutes exercised to total
+        int minutes = int.Parse(entry);
+        runningTotal = runningTotal + minutes;
 
-      // Display total minutes exercised to the screen
-      Console.WriteLine("You've entered " + runningTotal + " minutes.");
+        // Display total minutes exercised to the screen
+        Console.WriteLine("You've entered " + runningTotal + " minutes.");
 
-      //Repeat until user quits
+        //Repeat until user quits
+      }
+
     }
   }
 }
