76d357264fe46837198b97d15617294e2017bf9a
James Cho <hwisoocho@gmail.com>
2018-11-18 17:41:20 -0800

    try and catch exceptions

--- Exercise.cs
+++ Exercise.cs
@@ -21,27 +21,36 @@ namespace Fitness
         }
         else
         {
-          // Add minutes exercised to total
-          int minutes = int.Parse(entry);
+          try
+          {
 
-          if(minutes <= 10)
-          {
-            Console.WriteLine("Better than nothing, am I right?");
-          }
-          else if(minutes <=30)
-          {
-            Console.WriteLine("Way to go!");
-          }
-          else if(minutes <=60)
-          {
-            Console.WriteLine("You must be a ninja warrior in training!");
-          } 
-          else
-          {
-            Console.WriteLine("Okay, now you're just showing off!");
-          }
+            // Add minutes exercised to total
+            int minutes = int.Parse(entry);
+
+            if(minutes <= 10)
+            {
+              Console.WriteLine("Better than nothing, am I right?");
+            }
+            else if(minutes <=30)
+            {
+              Console.WriteLine("Way to go!");
+            }
+            else if(minutes <=60)
+            {
+              Console.WriteLine("You must be a ninja warrior in training!");
+            } 
+            else
+            {
+              Console.WriteLine("Okay, now you're just showing off!");
+            }
 
           runningTotal = runningTotal + minutes;
+          }
+          catch(FormatException)
+          {
+            Console.WriteLine("That is not a valid input.");
+            continue;
+          }
 
           // Display total minutes exercised to the screen
           Console.WriteLine("You've entered " + runningTotal + " minutes.");
